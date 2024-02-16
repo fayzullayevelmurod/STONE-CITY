a0a7e486d3e1d8260561b11ad93c0479668b0f03
QobilovSardor <note47297@gmail.com>
2024-02-16 22:51:54 +0500

    sd

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,53 @@ var swiper = new Swiper(".discount-swiper", {
 		prevEl: ".swiper-button-prev3",
 	},
 });
+
+const readMore = document.querySelector('.read-more');
+const readMoreText = document.querySelector('.hide-text');
+
+readMore.addEventListener('click', () => {
+	if (readMoreText.classList.contains('hidden')) {
+		readMore.textContent = 'Скрыть текст';
+		readMoreText.classList.remove('hidden');
+		readMoreText.classList.add('block');
+	} else {
+		readMore.textContent = 'Читать весь текст';
+		readMoreText.classList.remove('block');
+		readMoreText.classList.add('hidden');
+	}
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+// Manually set the first accordion to be active
+acc[0].classList.add("active");
+var firstPanel = acc[0].nextElementSibling;
+firstPanel.style.maxHeight = firstPanel.scrollHeight + "px";
+acc[0].querySelector('img').src = "./src/images/icons/minus.svg"; // Change the path accordingly
+
+// Add event listeners to the rest of the accordions
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		// Close all other accordions
+		for (var j = 0; j < acc.length; j++) {
+			if (j !== i) {
+				acc[j].classList.remove("active");
+				var otherPanel = acc[j].nextElementSibling;
+				otherPanel.style.maxHeight = null;
+				acc[j].querySelector('img').src = "./src/images/icons/pluse.svg";
+			}
+		}
+
+		// Toggle the clicked accordion
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+			this.querySelector('img').src = "./src/images/icons/pluse.svg";
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+			this.querySelector('img').src = "./src/images/icons/minus.svg"; // Change the path accordingly
+		}
+	});
+}
